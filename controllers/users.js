27e0724eb6919073ecb5354d26f4e323271b51f5
Alena Nikitina <alena.tkachenk@mail.ru>
2023-04-15 16:13:02 +0300

    feat: i try test

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 const User = require('../models/user'); // модель
-const { BAD_REQUEST, INTERNAL_SERVERE_ERROR } = require('../errors/errors_constants'); // errors
+const { BAD_REQUEST, INTERNAL_SERVERE_ERROR, NOT_FOUND } = require('../errors/errors_constants'); // errors
 
 // создаёт пользователя. post('/users', createUser)
 const createUser = (req, res) => {
@@ -32,7 +32,7 @@ const getUser = (req, res) => {
       if (error.name === 'ValidationError') {
         res.status(BAD_REQUEST).send({ message: 'Пользователь по указанному _id не найден.', error });
       } else if (typeof error !== 'string') {
-        res.status(BAD_REQUEST).send({ message: 'Получение пользователя с некорректным id', error });
+        res.status(NOT_FOUND).send({ message: 'Получение пользователя с некорректным id', error });
       } else {
         res.status(INTERNAL_SERVERE_ERROR).send({ message: 'Произошла ошибка', error });
       }
